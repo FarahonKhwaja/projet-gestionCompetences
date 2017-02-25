d1a1ba7d77288b5b24d5e662fa8137cfbc68f8cd
Philippe ROQUES-GEOFFROY <borgodoff@gmail.com>
2017-02-25 19:30:04 +0100

    Ajout lire fichier competences_personnel.csv

--- GestionCompetences/src/gestionFichiers/Main.java
+++ GestionCompetences/src/gestionFichiers/Main.java
@@ -10,6 +10,7 @@ import gestioncompetences.Personne;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,24 +22,28 @@ public class Main {
 
     public static void main(String[] args) {
         //lecture de CSV
-        String chemin = "C:\\Users\\phili\\Documents\\GitHub\\projet-gestionCompetences\\fichiers_projet\\liste_personnel.csv";
-        String chemin2 = "C:\\Users\\phili\\Documents\\GitHub\\projet-gestionCompetences\\fichiers_projet\\liste_competences.csv";
-        ArrayList<Personne> liste_personnel = new ArrayList<>();
-        ArrayList<Competence> liste_competences = new ArrayList<>();
+        /*String chemin = "C:\\Users\\phili\\Documents\\GitHub\\projet-gestionCompetences\\fichiers_projet\\liste_personnel.csv";
+        String chemin2 = "C:\\Users\\phili\\Documents\\GitHub\\projet-gestionCompetences\\fichiers_projet\\liste_competences.csv";*/
+        String chemin3 = "C:\\Users\\phili\\Documents\\GitHub\\projet-gestionCompetences\\fichiers_projet\\competences_personnel.csv";
+        /*ArrayList<Personne> liste_personnel = new ArrayList<>();
+        ArrayList<Competence> liste_competences = new ArrayList<>();*/
+        HashMap<Integer, ArrayList<String>> hashmap;
         try {
-            liste_personnel = lecteur.lireFichierPersonnes(chemin);
-            liste_competences = lecteur.lireFichierCompetences(chemin2);
+            /*liste_personnel = Lecteur.lireFichierPersonnes(chemin);
+            liste_competences = Lecteur.lireFichierCompetences(chemin2);*/
+            hashmap = Lecteur.lireFichierCompetencesParPersonne(chemin3);
+            //System.out.println(hashmap.toString());
         } catch (IOException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
         //ecriture dans un CSV
-        try {
-            writer.sauvegarderPersonnel("C:\\Users\\phili\\Documents\\GitHub\\projet-gestionCompetences\\fichiers_projet\\testPers.csv", liste_personnel);
-            writer.sauvegarderCompetences("C:\\Users\\phili\\Documents\\GitHub\\projet-gestionCompetences\\fichiers_projet\\testComp.csv", liste_competences);
+        /*try {
+            Writer.sauvegarderPersonnel("C:\\Users\\phili\\Documents\\GitHub\\projet-gestionCompetences\\fichiers_projet\\testPers.csv", liste_personnel);
+            Writer.sauvegarderCompetences("C:\\Users\\phili\\Documents\\GitHub\\projet-gestionCompetences\\fichiers_projet\\testComp.csv", liste_competences);
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        }*/
     }
 }

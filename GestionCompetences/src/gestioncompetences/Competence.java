d1e4ad388c25722ecf1a0f870d1f4cec6b05370d
Entrax <entrax.643@gmail.com>
2017-02-25 16:39:56 +0100

    Marche plz

--- GestionCompetences/src/gestioncompetences/Competence.java
+++ GestionCompetences/src/gestioncompetences/Competence.java
@@ -9,14 +9,57 @@ package gestioncompetences;
  *
  * @author phili
  */
-public class Competence {
-    int idCompetence;
-    String nomEN;
-    String nomFR;
-    
+class Competence {
+
+    private int idCompetence;
+    private String nomEN;
+    private String nomFR;
+
     Competence(int idComp, String nomAnglais, String nomFrancais) {
         idCompetence = idComp;
         nomEN = nomAnglais;
         nomFR = nomFrancais;
+    }
+
+    /**
+     * @return the idCompetence
+     */
+    public int getIdCompetence() {
+        return idCompetence;
+    }
+
+    /**
+     * @param idCompetence the idCompetence to set
+     */
+    public void setIdCompetence(int idCompetence) {
+        this.idCompetence = idCompetence;
+    }
+
+    /**
+     * @return the nomEN
+     */
+    public String getNomEN() {
+        return nomEN;
+    }
+
+    /**
+     * @param nomEN the nomEN to set
+     */
+    public void setNomEN(String nomEN) {
+        this.nomEN = nomEN;
+    }
+
+    /**
+     * @return the nomFR
+     */
+    public String getNomFR() {
+        return nomFR;
+    }
+
+    /**
+     * @param nomFR the nomFR to set
+     */
+    public void setNomFR(String nomFR) {
+        this.nomFR = nomFR;
     }
 }
--- GestionCompetences/src/gestioncompetences/MissionPlanifiee.java
+++ GestionCompetences/src/gestioncompetences/MissionPlanifiee.java
@@ -5,10 +5,36 @@
  */
 package gestioncompetences;
 
+import java.util.Date;
+import java.util.HashMap;
+
 /**
  *
  * @author phili
  */
-public class MissionPlanifiee implements Mission {
-    
+class MissionPlanifiee extends MissionPreparation {
+
+    HashMap<Competence, Integer> personelAffecte;
+
+    public MissionPlanifiee(Date dateDebut, int duree, int nbRequis, HashMap<Competence, Integer> personelAffecte) {
+        super(dateDebut, duree, nbRequis);
+        this.personelAffecte = personelAffecte;
+    }
+
+    public HashMap<Competence, Integer> getPersonelAffecte() {
+        return personelAffecte;
+    }
+
+    public void setPersonelAffecte(HashMap<Competence, Integer> personelAffecte) {
+        this.personelAffecte = personelAffecte;
+    }
+
+    public void addCompetence(Competence competence, int nbPersonne) {
+        this.personelAffecte.put(competence, nbPersonne);
+    }
+
+    public void removeCompetence(Competence competence) {
+        this.personelAffecte.remove(competence);
+    }
+
 }
